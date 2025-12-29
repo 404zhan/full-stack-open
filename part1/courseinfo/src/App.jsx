eb12a0ed4af99a81fbbc0eda9841b86327507dd2
404zhan <onedirection123124@gmail.com>
2025-12-29 15:57:16 +0530

    ex 2.1-2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,76 +1,86 @@
-import { useState } from 'react'
+const Sum = ({arr}) => {
+  return (
+    <p><strong>total of {arr.reduce((a, b)=>a+b.exercises, 0)} exercises</strong></p>
+  )
+}
 
-const StatisticLine = (props) => {
+const Part = ({topic}) => {
   return (
-    <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
-    </tr>
+    <li>{topic.name} {topic.exercises}</li>
   )
 }
-const Button = ({handleClick, text}) => {
+
+const Content = ({part}) => {
   return (
-    <button onClick = {handleClick}>{text}</button>
+    <ul>
+      {part.map(con => 
+       <Part key = {con.id} topic = {con}/>
+      )}
+    </ul>
   )
 }
-const Statistics = (props) => {
-  const total = props.good+props.neutral+props.bad
-  if (total == 0){
-    return (
-      <p>No feedback given</p>
-    )
-  }
-  const average = (props.good-props.bad)/total
-  const positive = props.good/total
+
+const Course = ({course}) => {
   return (
-      <table>
-        <tbody>
-          <StatisticLine text = "good" value = {props.good}/>
-          <StatisticLine text = "bad" value = {props.bad}/>
-          <StatisticLine text = "neutral" value = {props.neutral}/>
-          <StatisticLine text = "average" value = {average}/>
-          <StatisticLine text = "positive" value = {positive + '%'}/>
-        </tbody>
-      </table>
+    <div>
+      <h1>{course.name}</h1>
+      <Content part = {course.parts} key = {course.parts.id}/>
+      <Sum arr = {course.parts}/>
+    </div>
   )
 }
+
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
   ]
-
-  const [selected, setSelected] = useState(0)
-  const [vote, setVote] = useState(Array(anecdotes.length).fill(0))
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const handleVote = () => {
-    const copy = [...vote]
-    copy[selected] += 1
-    setVote(copy)
-  }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick = {() => setGood(good+1)} text = "good"/>
-      <Button handleClick = {() => setNeutral(neutral+1)} text = "neutral"/>
-      <Button handleClick = {() => setBad(bad+1)} text = "bad"/>
-      <h2>Anecdote of the day</h2>
-      <p>{anecdotes[selected]}</p>
-      <p>has {vote[selected]} votes</p>
-      <Button handleClick = {() => setSelected(Math.floor(Math.random()*anecdotes.length))} text = "next anecdote"/>
-      <Button handleClick={handleVote} text = "vote"/>
-      <h2>Anecdote with largest number of votes</h2>
-      <p>{anecdotes[vote.indexOf(Math.max(...vote))]} with {Math.max(...vote)}</p>
-      <Statistics good = {good} bad = {bad} neutral = {neutral}/>
+      {courses.map(course => 
+        <Course key = {course.id} course={course} />
+      )}
     </div>
   )
 }
